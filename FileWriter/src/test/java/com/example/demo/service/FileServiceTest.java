c22a07cfbdf2482f4da25d214d826fba413d170a
Mamy <xingyanjintian@gmail.com>
2025-02-14 17:16:49 +0900

    サービスクラスのテストを実施。異常なし。

---
+++ FileWriter/src/test/java/com/example/demo/service/FileServiceTest.java
@@ -0,0 +1,57 @@
+package com.example.demo.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.demo.entity.FileEntity;
+import com.example.demo.repository.FileRepository;
+
+@SpringBootTest
+class FileServiceTest {
+
+	@Autowired
+	private FileService fileService;
+
+	@Autowired
+	private FileRepository fileRepository;
+
+	@Test
+	@Transactional
+	@Rollback
+	void testSaveFile() throws IOException {
+		MultipartFile file = new MockMultipartFile("file","test_text.txt","text/plan","Hello!".getBytes());
+
+		String savedFileName = fileService.saveFile(file, true);
+		
+		Optional<FileEntity> savedFile = fileRepository.findByFileName(savedFileName);
+		assertTrue(savedFile.isPresent());
+		assertEquals("test_text.txt",savedFile.get().getFileName());
+	}
+	@Test
+	@Transactional
+	@Rollback
+	void deleteFile() throws IOException{
+		MultipartFile file = new MockMultipartFile("file","test.text","text/plan","Hello".getBytes());
+		
+		String FileName = fileService.saveFile(file,true);
+		
+		Optional<FileEntity> sevedFile = fileRepository.findByFileName(FileName);
+		
+		Long fileId = sevedFile.get().getId();
+		fileService.deleteFile(fileId);
+		
+		Optional<FileEntity> deleteFile = fileRepository.findById(fileId);
+		assertFalse(deleteFile.isPresent());
+		
+	}
+}
