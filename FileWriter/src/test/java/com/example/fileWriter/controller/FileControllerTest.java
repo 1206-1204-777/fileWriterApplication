e2c227715545aed9af14b825e41d6e4bd017ab78
Mamy <xingyanjintian@gmail.com>
2025-02-18 16:27:25 +0900

    テストコードの記述を開始

---
+++ FileWriter/src/test/java/com/example/fileWriter/controller/FileControllerTest.java
@@ -0,0 +1,14 @@
+package com.example.fileWriter.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FileControllerTest {
+
+	@Test
+	void test() {
+		fail("まだ実装されていません");
+	}
+
+}
---
+++ FileWriter/src/test/java/com/example/fileWriter/service/FileServiceTest.java
@@ -0,0 +1,14 @@
+package com.example.fileWriter.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FileServiceTest {
+
+	@Test
+	void test() {
+		fail("まだ実装されていません");
+	}
+
+}
